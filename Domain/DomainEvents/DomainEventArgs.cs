48842ac675b328381031f39e113191212a60dee8
Zeyad <zyabdelwahab1@sheffield.ac.uk>
2022-04-01 17:25:51 +0200

    Add Domain EventArgs.

---
+++ Domain/DomainEvents/DomainEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.DomainEvents
+{
+   public sealed class DomainEventArgs:EventArgs
+    {
+        public string ChangedDomainPropertyName { get; set; }
+        public string ChangedDomainPropertyNewValue { get; set; }
+
+    }
+}
