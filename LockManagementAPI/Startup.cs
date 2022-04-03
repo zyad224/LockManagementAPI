dcdb0af95e8316a76023fe555fbe288ea791f267
Zeyad <zyabdelwahab1@sheffield.ac.uk>
2022-04-03 02:33:08 +0200

    Trigger CI/CD Pipeline on Master.

--- LockManagementAPI/Startup.cs
+++ LockManagementAPI/Startup.cs
@@ -95,7 +95,7 @@ namespace LockManagementAPI
             services.AddControllers();
 
         }
-
+         
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
